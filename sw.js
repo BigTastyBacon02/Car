f54e4b2dd241f2973543fd99373791b13923efa3
BigTastyBacon02 <peterguenter4@gmx.de>
2025-10-02 02:43:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,3 @@
-const CACHE='v8ev-v2';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
-self.addEventListener('activate',e=>{ e.waitUntil(self.clients.claim()); });
-self.addEventListener('fetch',e=>{
-  const url=new URL(e.request.url);
-  if(url.origin===location.origin){
-    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-  }
-});
+const CACHE='v8ev-v3';
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./assets/transam.png','./assets/fiat.png','./assets/bg.png'])))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
