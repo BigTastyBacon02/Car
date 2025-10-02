6e2da05ff5a1cc6f4dc0b2f8f8fe865883922f0a
BigTastyBacon02 <peterguenter4@gmx.de>
2025-10-02 02:15:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,5 @@
-// Cache-first SW for GitHub Pages
-const CACHE='v8ev-v1';
-const ASSETS=[
-  './',
-  './index.html',
-  './manifest.webmanifest',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
-];
+const CACHE='v8ev-v2';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
 self.addEventListener('activate',e=>{ e.waitUntil(self.clients.claim()); });
 self.addEventListener('fetch',e=>{
