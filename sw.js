29ea4157b8488b3fbbfa0558ccc13eb96904042d
BigTastyBacon02 <peterguenter4@gmx.de>
2025-10-02 02:07:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,38 +1,17 @@
-// Minimaler Offline-Cache für GitHub Pages (v4)
-const CACHE = 'trans-am-75-v4';
-const ASSETS = [
+// Cache-first SW for GitHub Pages
+const CACHE='v8ev-v1';
+const ASSETS=[
   './',
   './index.html',
-  './manifest.webmanifest?v=4',
+  './manifest.webmanifest',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin) {
-    e.respondWith(
-      caches.match(e.request).then(res =>
-        res || fetch(e.request).then(r => {
-          const copy = r.clone();
-          caches.open(CACHE).then(c => c.put(e.request, copy));
-          return r;
-        }).catch(() => caches.match('./index.html'))
-      )
-    );
+self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())); });
+self.addEventListener('activate',e=>{ e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch',e=>{
+  const url=new URL(e.request.url);
+  if(url.origin===location.origin){
+    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
   }
 });
